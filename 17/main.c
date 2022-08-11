3805c8e8874f890dd60fa45abd6bf5a962aeec66
Yuri Geovani <yuri_geovani@hotmail.com>
2022-08-11 17:45:13 +0100

    Until class 18

--- 17/main.c
+++ 17/main.c
@@ -3,5 +3,26 @@
 
 int main(){
 
+    /*
+        == comparação
+        != diferença
+    */
+
+    int i = 10;
+//
+//    printf("%i\n", i==1); //Comparação se i é igual a 1
+//    printf("%i\n", i!=1); // Comparação se i é diferente de 1
+//
+//    printf("%i\n", !(i==1)); // Compara a negação do valor a 1.
+
+    printf("Digite um número != de 5: \n");
+    scanf("%i", &i);
+
+    if(i!=5){
+        printf("TRUE");
+    } else {
+        printf("FALSE");
+    }
+
     return 0;
 }
--- 18/main.c
+++ 18/main.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+
+void validarNota(float nota){
+
+    if(nota>10){
+        printf("Nova invalida! Insira um valor entre 0 e 10 (inclusives)!\n");
+        return false;
+    }
+    return true;
+}
+
 
 int main(){
 
+    int bimestres = 4;
+    float notas[bimestres], media;
+
+    printf("########## MEDIA ESCOLAR ##########\n");
+    printf("Informe as notas de cada bimestre:\n");
+
+    for(int i=0;i<bimestres;i++){
+        while()
+        printf("%i: ", i+1);
+        scanf("%f", &notas[i]);
+
+        validarNota(notas[i]);
+
+        media += notas[i];
+    }
+
+        printf("\nResultado: %f", (media/bimestres));
+//    scanf("%f", &num1);
+
+//    printf("\nO valor da soma eh %.2f.", validarNota(num1));
+
     return 0;
 }
+
+
