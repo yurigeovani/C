7b25b3291038dbcbed80aab001f605bfab3d5489
Yuri <yuri_geovani@hotmail.com>
2022-08-17 01:48:47 +0100

    Until class 20. Remove .o, main and .exe files.

--- 04/main.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void main () {
-    printf("Ola, mundo!");
-    return 0;
-}
--- 20/main.c
+++ 20/main.c
@@ -3,5 +3,37 @@
 
 int main(){
 
+    int i = 40;
+    int condicao = (i>20)&&(i<100);
+
+    printf("%i\n", condicao);
+    printf("%i\n", !condicao);
+
+    /*
+        ||
+        true || false = 1
+        false || true = 1
+        true || true = 1
+        false || false = 0
+    */
+
+    system("clear");
+    
+    int x = 10;
+    int cond = 0;
+
+    cond = (x==10) || (x<1);
+    printf("%i", cond);
+
+    cond = (x==0) || (x!=1000);
+    printf("%i", cond);
+
+    cond = (x==10) || (x!=0);
+    printf("%i", cond);
+
+    cond = (x!=10) || (x==1);
+    printf("%i", cond);
+
+
     return 0;
 }
