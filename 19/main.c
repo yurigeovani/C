8d03e871feb44072ba39e2bc4c27b11f2801ffaf
Yuri <yuri_geovani@hotmail.com>
2022-08-17 01:23:20 +0100

    Until class 19

--- 19/main.c
+++ 19/main.c
@@ -3,5 +3,21 @@
 
 int main(){
 
+    int iJ, iI;
+    iJ = 17;
+    iI = 60;
+
+    int idade = 0;
+
+    printf("Digite a idade de uma pessoa: ");
+    scanf("%i", &idade);
+
+    if(idade <= iJ){
+        printf("A idade informada eh de um jovem.\n");
+    } else if(idade >= iI){
+        printf("A idade informada eh de uma pessoa idosa.\n");
+    } else if(idade >iJ && idade <iI) {
+        printf("A idade informada eh de uma pessoa de meia idade.\n");
+    }
     return 0;
 }
